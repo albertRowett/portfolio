d7c8a651504c7afa03b26a55d08b928f6684a4b3
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-12-12 11:47:41 +0100

    Create tech stack logos banner with responsive styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         "albert-sans": ["Albert Sans", "sans-serif"],
       },
       spacing: {
+        7.5: "1.875rem",
+        15: "3.75rem",
         23: "5.75rem",
         140: "35rem",
         "4/5": "80%",
