297869b4e326bc426101614754861355d4b5bec0
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-12-13 19:48:26 +0100

    Add styled image to About section, replacing placeholder box

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {
       fontFamily: {
         "albert-sans": ["Albert Sans", "sans-serif"],
+        caveat: ["Caveat", "sans-serif"],
       },
       spacing: {
         7.5: "1.875rem",
         15: "3.75rem",
+        18.5: "4.625rem",
         23: "5.75rem",
         140: "35rem",
         "2/3": "66.667%",
@@ -17,6 +19,9 @@ module.exports = {
       borderWidth: {
         5: "5px",
         6: "6px",
+      },
+      lineHeight: {
+        18.5: "4.625rem",
       },
     },
   },
